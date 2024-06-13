5c78b7841c07e199cae05026bac29fb257cca2bf
harshitjaiin <harshitjain.tj@gmail.com>
2024-06-13 13:53:07 +0530

    Updated CORS origin

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const config = require('./config/config');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-
 const app = express();
 const port = process.env.port || 3000
 
 // Use CORS middleware
-app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
-
+app.use(cors({
+    origin: 'https://regnum-web.vercel.app' // Allow requests only from this origin
+  }));
 mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Could not connect to MongoDB:', error));
