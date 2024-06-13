206bf4870ef43853c95ef033a47d238233270368
harshitjaiin <harshitjain.tj@gmail.com>
2024-06-13 14:30:31 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const { joinWaitlist, verifyMail, verifyOTP } = require('../controllers/userController');
+const cors = require('cors'); // Import CORS middleware
 
 const router = express.Router();
 
-router.get('/' , (req, res)=>{
+// CORS middleware for all routes in this router
+router.use(cors());
+
+router.get('/', (req, res) => {
     res.status(201).json({
-        "msg" : "working alright!"
-    })
-})
+        "msg": "working alright!"
+    });
+});
+
 router.post('/join-waitlist', joinWaitlist);
 router.get('/verify-email', verifyMail);
 router.get('/verify-otp', verifyOTP);
