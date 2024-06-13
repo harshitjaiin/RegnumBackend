b1d8dbf934cc405de0aa59161a7844637538db71
harshitjaiin <harshitjain.tj@gmail.com>
2024-06-13 13:36:02 +0530

    Port added

--- server.js
+++ server.js
@@ -3,31 +3,19 @@ const mongoose = require('mongoose');
 const config = require('./config/config');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-const path = require('path');  // Import path module
 
 const app = express();
-const port = process.env.PORT || 3000;  // Ensure port is defined
+const port = process.env.port || 3000
 
 // Use CORS middleware
 app.use(cors());
 app.use(express.json());
+app.use(userRoutes);
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname, '..')));
-
-// Use user routes
-app.use('/api', userRoutes);
-
-// Fallback route to serve index.html for any undefined routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
-
-// Connect to MongoDB
 mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Could not connect to MongoDB:', error));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Could not connect to MongoDB:', error));
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
